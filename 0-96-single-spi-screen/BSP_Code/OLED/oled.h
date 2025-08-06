2b7fbb9308fdc16ce10bfe50ffe32ff28f55c27a
LeenixP <lipeng.git@qq.com>
2025-08-06 11:36:56 +0800

    更新【0.96寸SPI单色屏】

---
+++ 0-96-single-spi-screen/BSP_Code/OLED/oled.h
@@ -0,0 +1,82 @@
+#ifndef __OLED_H
+#define __OLED_H 
+
+#include "gd32vw55x.h"
+#include "systick.h"
+
+#ifndef delay_ms
+#define delay_ms(x)  delay_1ms(x)
+#endif
+
+#ifndef delay_us
+#define delay_us(x)  delay_1us(x)
+#endif
+
+
+#ifndef u8
+#define u8  uint8_t
+#endif
+#ifndef u16
+#define u16 uint16_t
+#endif
+#ifndef u32
+#define u32 uint32_t
+#endif
+
+//-----------------OLED端口定义----------------
+#define OLED_RCU_ENABLE()   rcu_periph_clock_enable(RCU_GPIOA); \
+                            rcu_periph_clock_enable(RCU_GPIOB); \
+                            rcu_periph_clock_enable(RCU_SPI);
+
+#define OLED_SCL_PORT    GPIOA
+#define OLED_SCL_PIN     GPIO_PIN_11
+#define OLED_SCL_AF      GPIO_AF_0
+
+#define OLED_SDA_PORT    GPIOA
+#define OLED_SDA_PIN     GPIO_PIN_9
+#define OLED_SDA_AF      GPIO_AF_0
+
+#define OLED_RES_PORT    GPIOA
+#define OLED_RES_PIN     GPIO_PIN_7
+
+#define OLED_DC_PORT     GPIOA
+#define OLED_DC_PIN      GPIO_PIN_6
+
+#define OLED_CS_PORT     GPIOB
+#define OLED_CS_PIN      GPIO_PIN_11
+
+
+#define OLED_RES_Clr()  gpio_bit_write(OLED_RES_PORT, OLED_RES_PIN, 0)//RES
+#define OLED_RES_Set()  gpio_bit_write(OLED_RES_PORT, OLED_RES_PIN, 1)
+
+#define OLED_DC_Clr()   gpio_bit_write(OLED_DC_PORT, OLED_DC_PIN, 0)//DC
+#define OLED_DC_Set()   gpio_bit_write(OLED_DC_PORT, OLED_DC_PIN, 1)
+
+#define OLED_CS_Clr()   gpio_bit_write(OLED_CS_PORT, OLED_CS_PIN, 0)//CS
+#define OLED_CS_Set()   gpio_bit_write(OLED_CS_PORT, OLED_CS_PIN, 1)
+
+#define OLED_CMD  0	//写命令
+#define OLED_DATA 1	//写数据
+
+void OLED_ClearPoint(u8 x,u8 y);
+void OLED_ColorTurn(u8 i);
+void OLED_DisplayTurn(u8 i);
+void OLED_WR_Byte(u8 dat,u8 mode);
+void OLED_DisPlay_On(void);
+void OLED_DisPlay_Off(void);
+void OLED_Refresh(void);
+void OLED_Clear(void);
+void OLED_DrawPoint(u8 x,u8 y,u8 t);
+void OLED_DrawLine(u8 x1,u8 y1,u8 x2,u8 y2,u8 mode);
+void OLED_DrawCircle(u8 x,u8 y,u8 r);
+void OLED_ShowChar(u8 x,u8 y,u8 chr,u8 size1,u8 mode);
+void OLED_ShowChar6x8(u8 x,u8 y,u8 chr,u8 mode);
+void OLED_ShowString(u8 x,u8 y,u8 *chr,u8 size1,u8 mode);
+void OLED_ShowNum(u8 x,u8 y,u32 num,u8 len,u8 size1,u8 mode);
+void OLED_ShowChinese(u8 x,u8 y,u8 num,u8 size1,u8 mode);
+void OLED_ScrollDisplay(u8 num,u8 space,u8 mode);
+void OLED_ShowPicture(u8 x,u8 y,u8 sizex,u8 sizey,u8 BMP[],u8 mode);
+void OLED_Init(void);
+
+#endif
+
---
+++ 0-96-single-spi-screen/src/main.c
@@ -0,0 +1,116 @@
+#include "gd32vw55x.h"
+#include "systick.h"
+#include <stdio.h>
+#include "main.h"
+#include "gd32vw553h_eval.h"
+
+#include "oled.h"
+#include "bmp.h"
+
+/*!
+    \brief      toggle the led every 500ms
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+void led_spark(void)
+{
+    static __IO uint32_t timingdelaylocal = 0U;
+
+    if(timingdelaylocal) {
+
+        if(timingdelaylocal < 500U) {
+            gd_eval_led_on(LED1);
+        } else {
+            gd_eval_led_off(LED1);
+        }
+
+        timingdelaylocal--;
+    } else {
+        timingdelaylocal = 1000U;
+    }
+}
+
+/*!
+    \brief      main function
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+int main(void)
+{
+#ifdef __FIRMWARE_VERSION_DEFINE
+    uint32_t fw_ver = 0;
+#endif /* __FIRMWARE_VERSION_DEFINE */
+
+    /* configure systick */
+    systick_config();
+    eclic_priority_group_set(ECLIC_PRIGROUP_LEVEL3_PRIO1);
+    /* initilize the LEDs, USART and key */
+    gd_eval_led_init(LED1);
+    gd_eval_com_init(EVAL_COM0);
+    gd_eval_key_init(KEY_TAMPER_WAKEUP, KEY_MODE_GPIO);
+
+#ifdef __FIRMWARE_VERSION_DEFINE
+    fw_ver = gd32vw55x_firmware_version_get();
+    printf("\r\n= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =\r\n\n");
+    printf("\r\n=== Welcome to use the LC-GD32VW553-HMQ6 development board ====\r\n\n");
+    printf("\r\n======================= www.lckfb.com =========================\r\n\n");
+    printf("\r\n======================= wiki.lckfb.com ========================\r\n\n");
+    printf("\r\n= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =\r\n");
+    /* print firmware version */
+    printf("\r\nGD32VW55X series firmware version: V%d.%d.%d", (uint8_t)(fw_ver >> 24), (uint8_t)(fw_ver >> 16), (uint8_t)(fw_ver >> 8));
+#endif /* __FIRMWARE_VERSION_DEFINE */
+
+    /* print out the clock frequency of system, AHB, APB1 and APB2 */
+    printf("\r\nCK_SYS is %d\r\n", rcu_clock_freq_get(CK_SYS));
+    printf("\r\nCK_AHB is %d\r\n", rcu_clock_freq_get(CK_AHB));
+    printf("\r\nCK_APB1 is %d\r\n", rcu_clock_freq_get(CK_APB1));
+    printf("\r\nCK_APB2 is %d\r\n", rcu_clock_freq_get(CK_APB2));
+
+    uint8_t t=' ';
+
+    OLED_Init();
+    OLED_ColorTurn(0);//0正常显示，1 反色显示
+    OLED_DisplayTurn(0);//0正常显示 1 屏幕翻转显示
+
+    while(1)
+    {
+        OLED_ShowPicture(0,0,128,64,BMP1,1);
+        OLED_Refresh();
+        delay_1ms(500);
+        OLED_Clear();
+        OLED_ShowChinese(0,0,0,16,1);//中
+        OLED_ShowChinese(18,0,1,16,1);//景
+        OLED_ShowChinese(36,0,2,16,1);//园
+        OLED_ShowChinese(54,0,3,16,1);//电
+        OLED_ShowChinese(72,0,4,16,1);//子
+        OLED_ShowChinese(90,0,5,16,1);//技
+        OLED_ShowChinese(108,0,6,16,1);//术
+        OLED_ShowString(8,16,"ZHONGJINGYUAN",16,1);
+        OLED_ShowString(20,32,"2014/05/01",16,1);
+        OLED_ShowString(0,48,"ASCII:",16,1);
+        OLED_ShowString(63,48,"CODE:",16,1);
+        OLED_ShowChar(48,48,t,16,1);//显示ASCII字符
+        t++;
+        if(t>'~')t=' ';
+        OLED_ShowNum(103,48,t,3,16,1);
+        OLED_Refresh();
+        delay_1ms(500);
+        OLED_Clear();
+        OLED_ShowChinese(0,0,0,16,1);  //16*16 中
+        OLED_ShowChinese(16,0,0,24,1); //24*24 中
+        OLED_ShowChinese(24,20,0,32,1);//32*32 中
+        OLED_ShowChinese(64,0,0,64,1); //64*64 中
+        OLED_Refresh();
+        delay_1ms(500);
+        OLED_Clear();
+        OLED_ShowString(0,0,"ABC",8,1);//6*8 “ABC”
+        OLED_ShowString(0,8,"ABC",12,1);//6*12 “ABC”
+        OLED_ShowString(0,20,"ABC",16,1);//8*16 “ABC”
+        OLED_ShowString(0,36,"ABC",24,1);//12*24 “ABC”
+        OLED_Refresh();
+        delay_1ms(500);
+        OLED_ScrollDisplay(11,4,1);
+    }
+}
