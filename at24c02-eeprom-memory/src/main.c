e0649bfc1ff27fc63679ff47f89db0e75b272e54
LeenixP <lipeng.git@qq.com>
2025-08-13 16:17:24 +0800

    更新【AT24C02-EEPROM存储器】模块

---
+++ at24c02-eeprom-memory/src/main.c
@@ -0,0 +1,145 @@
+/*!
+    \file    main.c
+    \brief   led spark with systick, USART print and key example
+
+    \version 2025-01-16, V1.4.0, firmware for GD32VW55x
+*/
+
+/*
+    Copyright (c) 2025, GigaDevice Semiconductor Inc.
+
+    Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this
+       list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
+       and/or other materials provided with the distribution.
+    3. Neither the name of the copyright holder nor the names of its contributors
+       may be used to endorse or promote products derived from this software without
+       specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+OF SUCH DAMAGE.
+*/
+
+#include "gd32vw55x.h"
+#include "systick.h"
+#include <stdio.h>
+#include "main.h"
+#include "gd32vw553h_eval.h"
+
+#include "bsp_at24c02.h"
+
+/*!
+    \brief      toggle the led every 500ms
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+void led_spark(void)
+{
+    static __IO uint32_t timingdelaylocal = 0U;
+
+    if(timingdelaylocal) {
+
+        if(timingdelaylocal < 500U) {
+            gd_eval_led_on(LED1);
+        } else {
+            gd_eval_led_off(LED1);
+        }
+
+        timingdelaylocal--;
+    } else {
+        timingdelaylocal = 1000U;
+    }
+}
+
+/*!
+    \brief      main function
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+int main(void)
+{
+#ifdef __FIRMWARE_VERSION_DEFINE
+    uint32_t fw_ver = 0;
+#endif /* __FIRMWARE_VERSION_DEFINE */
+
+    /* configure systick */
+    systick_config();
+    eclic_priority_group_set(ECLIC_PRIGROUP_LEVEL3_PRIO1);
+    /* initilize the LEDs, USART and key */
+    gd_eval_led_init(LED1);
+    gd_eval_com_init(EVAL_COM0);
+    gd_eval_key_init(KEY_TAMPER_WAKEUP, KEY_MODE_GPIO);
+
+#ifdef __FIRMWARE_VERSION_DEFINE
+    fw_ver = gd32vw55x_firmware_version_get();
+    printf("\r\n= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =\r\n\n");
+    printf("\r\n=== Welcome to use the LC-GD32VW553-HMQ6 development board ====\r\n\n");
+    printf("\r\n======================= www.lckfb.com =========================\r\n\n");
+    printf("\r\n======================= wiki.lckfb.com ========================\r\n\n");
+    printf("\r\n= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =\r\n");
+    /* print firmware version */
+    printf("\r\nGD32VW55X series firmware version: V%d.%d.%d", (uint8_t)(fw_ver >> 24), (uint8_t)(fw_ver >> 16), (uint8_t)(fw_ver >> 8));
+#endif /* __FIRMWARE_VERSION_DEFINE */
+
+    /* print out the clock frequency of system, AHB, APB1 and APB2 */
+    printf("\r\nCK_SYS is %d\r\n", rcu_clock_freq_get(CK_SYS));
+    printf("\r\nCK_AHB is %d\r\n", rcu_clock_freq_get(CK_AHB));
+    printf("\r\nCK_APB1 is %d\r\n", rcu_clock_freq_get(CK_APB1));
+    printf("\r\nCK_APB2 is %d\r\n", rcu_clock_freq_get(CK_APB2));
+
+    /* initialize the AT24C02 EEPROM */
+    AT24C02_Init();
+    printf("\r\nAT24C02 EEPROM initialized.\r\n");
+
+    uint8_t dat_1 = 0;
+    uint8_t dat_2 = 0;
+
+    printf("\r\nAT24C02 Demo Start.....\r\n");
+
+    //向0地址写入数据48
+    AT24C02_WriteByte(0,48);
+    delay_1ms(10);
+
+    //向8地址写入数据48
+    AT24C02_WriteByte(8,66);
+    delay_1ms(10);
+
+    //从0地址读取数据到dat1
+    dat_1 = AT24C02_ReadByte(0);
+    delay_1ms(10);
+    //从8地址读取数据到dat2
+    dat_2 = AT24C02_ReadByte(8);
+    delay_1ms(30);
+
+    // 输出dat查看数据是否正确
+    printf("\r\nData 1 = %d\r\n", dat_1);
+    printf("\r\nData 2 = %d\r\n", dat_2);
+
+    // 下面代码和上面代码无关，下面只是为了演示按键功能
+
+    while(1) 
+    {
+        if(RESET == gd_eval_key_state_get(KEY_TAMPER_WAKEUP)) 
+        {
+            delay_1ms(50);
+            if(SET == gd_eval_key_state_get(KEY_TAMPER_WAKEUP)) 
+            {
+                gd_eval_led_toggle(LED1);
+            }
+        }
+    }
+}
